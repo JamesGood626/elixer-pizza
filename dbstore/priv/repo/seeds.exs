6374e175d1508304c278df08a3abc1a15b38e553
James <jamesgood626@gmail.com>
2019-06-27 13:03:08 -0400

    Added starter database setup in /dbstore/priv/repo/seeds.exs

---
+++ dbstore/priv/repo/seeds.exs
@@ -0,0 +1,33 @@
+alias Dbstore.Repo
+alias Dbstore.{Pizza, Toppings, Permissions}
+
+Repo.insert!(%Permissions{name: "PIZZA_OPERATION_MANAGER"})
+Repo.insert!(%Permissions{name: "PIZZA_CHEF"})
+
+pizza = Repo.insert!(%Pizza{name: "Cheese Olive"})
+pepperoni = Repo.insert!(%Toppings{name: "Pepperoni"})
+pineapple = Repo.insert!(%Toppings{name: "Pineapple"})
+olives = Repo.insert!(%Toppings{name: "Olives"})
+
+Repo.insert_all("pizza_toppings", [
+  [
+    pizza_id: pizza.id,
+    topping_id: pepperoni.id,
+    inserted_at: ~N[2019-06-27 16:54:00],
+    updated_at: ~N[2019-06-27 16:54:00]
+  ],
+  [
+    pizza_id: pizza.id,
+    topping_id: pineapple.id,
+    inserted_at: ~N[2019-06-27 16:54:00],
+    updated_at: ~N[2019-06-27 16:54:00]
+  ]
+  [
+    pizza_id: pizza.id,
+    topping_id: olives.id,
+    inserted_at: ~N[2019-06-27 16:54:00],
+    updated_at: ~N[2019-06-27 16:54:00]
+  ],
+])
+
+IO.puts("Success! Sample data has been added.")
